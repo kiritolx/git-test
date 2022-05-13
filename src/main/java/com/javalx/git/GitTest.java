12951c9971e9f90e3881d5f0d2435c8bf549b279
link <link@qq.com>
2022-05-13 15:50:13 +0800

    first commit

---
+++ src/main/java/com/javalx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.javalx.git;
+
+/**
+ * @author javalx_kirito
+ * @version 1.0
+ * @create 2022-05-13 15:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
